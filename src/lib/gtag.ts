90f7f68f8e47a3a1e3ee3455a14f2cff19cea142
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-01-24 09:33:26 +0000

    test

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -31,3 +31,16 @@ export const event = ({
     value,
   });
 };
+
+
+//<!-- Global site tag (gtag.js) - Google Analytics -->
+//<script async src="https://www.googletagmanager.com/gtag/js?id=UA-216787896-1"></script>
+//<script>
+//  window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+//  gtag('js', new Date());
+//
+//  gtag('config', 'UA-216787896-1');
+//</script>
+
+// gtag('set', {'user_id': 'USER_ID'}); // Set the user ID using signed-in user_id.
